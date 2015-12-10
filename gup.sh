4b11d03e1412e669bce879051622545616fa9240
Joel Marshall <joel@tusksoft.com>
2015-12-10 11:30:25 -0500

    whitespace

--- gup.sh
+++ gup.sh
@@ -5,17 +5,17 @@ BEFORE=$(git stash list)
 git stash save gup-temporary-stash
 if [ $# -ne 1 ];
 then
-git checkout $2
-git fetch --all --prune
-git rebase -p origin/$2 
-git checkout $1
-git rebase -p $2
+	git checkout $2
+	git fetch --all --prune
+	git rebase -p origin/$2 
+	git checkout $1
+	git rebase -p $2
 else
-git checkout $1
-git fetch --all --prune
-git rebase -p origin/$1 
+	git checkout $1
+	git fetch --all --prune
+	git rebase -p origin/$1 
 fi
 if [ "$BEFORE" != "$(git stash list)" ]; 
 then
-git stash pop
+	git stash pop
 fi
