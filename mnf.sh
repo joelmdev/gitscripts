6b1fb5e3c419835b8603302f3e2863930f055819
Joel Marshall <joel@tusksoft.com>
2016-01-13 08:40:05 -0500

    Fixing copyright typos

--- mnf.sh
+++ mnf.sh
@@ -2,7 +2,7 @@
 
 #The MIT License (MIT)
 
-#Copyright (c) 2015 Joel Marshall - Tusk Software - http:#tusksoft.com
+#Copyright (c) 2015 Joel Marshall - Tusk Software - https://tusksoft.com
 
 #Permission is hereby granted, free of charge, to any person obtaining a copy
 #of this software and associated documentation files (the "Software"), to deal
